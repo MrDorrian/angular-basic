c249cc3e4c3c0cdc709c4bcf4f0586177d35776e
MrDorrian <mdundukov@gmail.com>
2021-05-14 14:12:06 +0300

    post - postform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {title: 'Lorem ipsum dolor sit amet. Nam, similique.', text: 'Lorem ipsum dolor sit amet. Nam, similique.', id: 1},
+    {title: 'Lorem ipsum dolor sit amet. Nam, similique2.', text: 'Lorem ipsum dolor sit amet. Nam, similique2.', id: 1}
+  ];
 
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+  }
 }
