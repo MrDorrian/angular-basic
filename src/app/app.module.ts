415d9c820ad6a9daaae10fe52301380b21f99b5f
MrDorrian <mdundukov@gmail.com>
2021-05-14 13:43:53 +0300

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {PostComponent} from './post/post.component';
-import {Post2Component} from './post2/post.component';
 import {FormsModule} from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostFormComponent,
     PostComponent,
-    Post2Component,
   ],
   imports: [
     BrowserModule,
