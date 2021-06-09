cf5eb4bc52448a65266c7dd8d83ba063c6a8d678
MrDorrian <mdundukov@gmail.com>
2021-06-09 10:36:16 +0300

    add form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,36 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Promise resolved');
-    }, 4000);
-  });
+export class AppComponent implements OnInit {
+  form: FormGroup;
 
-  date: Observable<Date> = new Observable(obs => {
-    setInterval(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl()
+      })
+    });
+  }
 
+  submit(): void {
+    console.log('Form', this.form);
+    const formData = {...this.form.value};
+    console.log(formData);
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,18 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
-import { NewPipePipe } from './pipes/new-pipe.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewPipePipe,
-    FilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
