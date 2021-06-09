69288b9f52ff2ac802f6aa73bb3eefca7a34b49b
MrDorrian <mdundukov@gmail.com>
2021-06-09 13:43:44 +0300

    first fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,8 +13,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
-
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(response => {
+        this.todos = response;
+      });
   }
 }
